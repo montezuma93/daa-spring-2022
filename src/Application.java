cb073058cf71ad4709a24111f7b811f33b8ad19e
montezuma93 <hmr@riki5.de>
2022-04-29 12:32:05 +0200

    Add new Application Class

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+
+    public static void main(String args[]) {
+        String message = "Hello";
+        int age = 18;
+        double size = 1.70;
+        boolean doIHaveHave = true;
+    }
+
+}
