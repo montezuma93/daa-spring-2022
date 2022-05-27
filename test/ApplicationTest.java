3c5868d1dbc283e1b302ac58275722bcd136633c
LinaKrishtal <anilochka93@gmail.com>
2022-05-27 09:53:39 +0200

    Änderungen am 27.05 von Alina

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,29 +1,30 @@
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
-class ApplicationTest {
+public class ApplicationTest {
 
     @Test
-    void testCalculateSum() {
+    public void testCalculateSum() {
         Assertions.assertEquals(3, Application.calculateSum(1,2));
         Assertions.assertEquals(4, Application.calculateSum(2,2));
         Assertions.assertEquals(2, Application.calculateSum(0,2));
-        Assertions.assertEquals(1, Application.calculateSum(-1,2));
-    }@Test
-    void testcalculatequotient(){
-        Assertions.assertEquals(3, Application.calculateQuotient(6,2) );
-        Assertions.assertEquals(4, Application.calculateQuotient(8,2) );
-        Assertions.assertEquals(2.5, Application.calculateQuotient(5,2) );
-        Assertions.assertEquals(15, Application.calculateQuotient(45,3) );
-        Assertions.assertEquals(7, Application.calculateQuotient(14,2) );
-        Assertions.assertEquals(0 , Application.calculateQuotient(0,4) );
-        Assertions.assertThrows(RuntimeException.class, ()->Application.calculateQuotient(5,0)  );
+        Assertions.assertEquals(-1, Application.calculateSum(-1,2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testCalculateDivision() {
+        Assertions.assertEquals(2, Application.calculateDivision(4,2));
+        Assertions.assertEquals(2.5, Application.calculateDivision(5,2));
+        Assertions.assertEquals(5, Application.calculateDivision(10,2));
+        Assertions.assertEquals(0, Application.calculateDivision(0,2));
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> Application.calculateDivision(4,0));
+        Assertions.assertEquals("test", exception.getMessage());
     }
     @Test
-    void testSubtract() {
-        Assertions.assertEquals(2, Application.subtractTwoNumbers(5,3));
-        Assertions.assertEquals(3, Application.subtractTwoNumbers(6,3));
-        Assertions.assertEquals(4, Application.subtractTwoNumbers(7,3));
-        Assertions.assertEquals(5, Application.subtractTwoNumbers(8,3));
+    public void testSubtract() {
+        Assertions.assertEquals(2, Application.calculateDifference(5,3));
+        Assertions.assertEquals(3, Application.calculateDifference(6,3));
+        Assertions.assertEquals(4, Application.calculateDifference(7,3));
+        Assertions.assertEquals(5, Application.calculateDifference(8,3));
     }
 }
